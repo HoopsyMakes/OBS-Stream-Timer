2a5ae28c90f705f9fe48867263a3962c9e939a5a
HoopsyMakes <hoopsymakes@proton.me>
2026-05-03 21:08:07 +0300

    Fix GitHub Pages routing basename

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import CountDown from './pages/CountDown';
 
-
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/countdown/:endTime' element={<CountDown />} />
