84dbaabd765b2b32af96fb3dc21e0bd4e605ba4b
HoopsyMakes <hoopsymakes@proton.me>
2026-05-03 21:24:39 +0300

    Use hash routing for GitHub Pages deep links

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import CountDown from './pages/CountDown';
 
-
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/countdown/:endTime' element={<CountDown />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,31 +1,37 @@
 import { useState } from 'react'
-function Home() {
-  const [h, setH] = useState(0);
-  const [m, setM] = useState(0);
-  const [s, setS] = useState(0);
 
+function Home() {
+  const [h, setH] = useState(0)
+  const [m, setM] = useState(0)
+  const [s, setS] = useState(0)
+
+  const buildCountdownUrl = (time: number) => {
+    const basePath = import.meta.env.BASE_URL
+    return `${window.location.origin}${basePath}#/countdown/${time}`
+  }
 
   const submitTime = async () => {
-    const time: number = Date.now() + 1000 * (h * 3600 + m * 60 + s);
-    await navigator.clipboard.writeText(window.location.href + "countdown/" + time.toString())
+    const time = Date.now() + 1000 * (h * 3600 + m * 60 + s)
+    await navigator.clipboard.writeText(buildCountdownUrl(time))
   }
+
   const submitDate = async () => {
-    const time = new Date(Date.now());
-    time.setHours(h, m, s, 0);
-    await navigator.clipboard.writeText(window.location.href + "/countdown/" + time.getTime());
+    const time = new Date(Date.now())
+    time.setHours(h, m, s, 0)
+    await navigator.clipboard.writeText(buildCountdownUrl(time.getTime()))
   }
 
   return (
     <div>
       <form>
         <label>Hours: </label>
-        <input value={h} onChange={(e) => { setH(parseInt(e.target.value)) }}></input>
+        <input value={h} onChange={(e) => { setH(Number(e.target.value) || 0) }}></input>
         <br />
         <label>Minutes: </label>
-        <input value={m} onChange={(e) => { setM(parseInt(e.target.value)) }}></input>
+        <input value={m} onChange={(e) => { setM(Number(e.target.value) || 0) }}></input>
         <br />
         <label>Seconds: </label>
-        <input value={s} onChange={(e) => { setS(parseInt(e.target.value)) }}></input>
+        <input value={s} onChange={(e) => { setS(Number(e.target.value) || 0) }}></input>
         <br />
       </form>
       <button onClick={submitTime}>Time from now</button>
